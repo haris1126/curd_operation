d27a4151b06260f3ff3804b472f39af94f40b0b5
Haris Mushtaq <harismushtaq93@gmail.com>
2022-06-16 17:17:44 +0530

    this is fifth commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,5 +3,5 @@ const mongoose = require('mongoose')
 const db = "mongodb+srv://root:root@cluster0.umnil.mongodb.net/menSchema?retryWrites=true&w=majority"
 
 mongoose.connect(db)
-.then(() => console.log("Connection is Established Succesfully..."))
+.then(() => console.log("Connection  is Established Succesfully mongodb..."))
     .catch((err) => console.log(err))
